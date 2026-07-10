d28f806bc01a8e80272f31e79eb7f19b1397fddf
TejKarthik40 <23501a4440@pvpsit.ac.in>
2026-07-10 19:14:03 +0530

    Added JWT o/p

--- week-3/spring-jwt-authentication/src/main/java/com/cognizant/springlearn/controller/AuthenticationController.java
+++ week-3/spring-jwt-authentication/src/main/java/com/cognizant/springlearn/controller/AuthenticationController.java
@@ -1,11 +1,17 @@
 package com.cognizant.springlearn.controller;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.crypto.SecretKey;
+import java.util.Base64;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,18 +20,38 @@ public class AuthenticationController {
 
     private static final Logger LOGGER =
             LoggerFactory.getLogger(AuthenticationController.class);
-
+    private static final String SECRET_KEY =
+            "ThisIsMyVerySecretKeyForJwtAuthentication123456";
     @GetMapping("/authenticate")
     public Map<String, String> authenticate(
             @RequestHeader("Authorization") String authHeader) {
 
         LOGGER.info("START");
 
-        LOGGER.debug("Authorization Header : {}", authHeader);
+        // Remove "Basic "
+        String encodedCredentials = authHeader.substring(6);
+
+        // Decode Base64
+        byte[] decodedBytes = Base64.getDecoder().decode(encodedCredentials);
+        String credentials = new String(decodedBytes);
+
+        // credentials = "user:pwd"
+        String username = credentials.split(":")[0];
+
+        LOGGER.debug("Username : {}", username);
+
+
+        SecretKey key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+        // Generate JWT
+        String token = Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 600000))
+                .signWith(SignatureAlgorithm.HS256, key)
+                .compact();
 
         Map<String, String> map = new HashMap<>();
-
-        map.put("token", "");
+        map.put("token", token);
 
         LOGGER.info("END");
 
